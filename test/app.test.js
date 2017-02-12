5136a48175d4ae0ab7b0d28bc4edafb9fd0d71a6
Richard Kiene <richard.kiene@joyent.com>
2017-02-12 19:20:59 +0000

    Add deploy-to script which makes deploying apm builds easy

--- test/app.test.js
+++ test/app.test.js
@@ -28,7 +28,7 @@ var DEFAULT_CONFIG = {
     port: 9090 /* 9090 chosen to not conflict with a running cmon-agent */
 };
 
-var DEFAULT_OPTS = { config: DEFAULT_CONFIG, log: log };
+var DEFAULT_OPTS = { config: DEFAULT_CONFIG, log: log, ip: '127.0.0.1' };
 
 test('create app succeeds', function _test(t) {
     var app;
@@ -50,7 +50,7 @@ test('create app succeeds', function _test(t) {
 test('create app fails with bad or no opts', function _test(t) {
     var app;
 
-    t.plan(4);
+    t.plan(11);
 
     t.throws(function _noOpts() {
         app = new lib_app();
@@ -58,9 +58,30 @@ test('create app fails with bad or no opts', function _test(t) {
     t.throws(function _emptyOpts() {
         app = new lib_app({});
     }, 'opts.config');
-    t.throws(function _noLogOpts() {
+    t.throws(function _noLogLevel() {
         app = new lib_app({ config: {} });
+    }, 'opts.config.logLevel');
+    t.throws(function _badLogLevel() {
+        app = new lib_app({ config: { logLevel: 1 } });
+    }, 'opts.config.logLevel');
+    t.throws(function _noPort() {
+        app = new lib_app({ config: { logLevel: 'DEBUG' } });
+    }, 'opts.config.port');
+    t.throws(function _badPort() {
+        app = new lib_app({ config: { logLevel: 'DEBUG', port: 'abc' } });
+    }, 'opts.config.port');
+    t.throws(function _noLog() {
+        app = new lib_app({ config: DEFAULT_CONFIG });
     }, 'opts.log');
+    t.throws(function _badLog() {
+        app = new lib_app({ config: DEFAULT_CONFIG, log: 'log' });
+    }, 'opts.log');
+    t.throws(function _noIp() {
+        app = new lib_app({ config: DEFAULT_CONFIG, log: log });
+    }, 'opts.ip');
+    t.throws(function _badIp() {
+        app = new lib_app({ config: DEFAULT_CONFIG, log: log, ip: 12345 });
+    }, 'opts.ip');
     t.notOk(app, 'app was not created');
 
     t.end();
